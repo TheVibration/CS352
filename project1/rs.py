927342d2349be62354a801937cf22153be3ac46d
Harsh <Harshabar9@gmail.com>
2020-02-23 13:14:08 -0500

    issues with going through lst

--- project1/rs.py
+++ project1/rs.py
@@ -27,14 +27,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as rs:
     # accept incoming connections
     csockid, addr = rs.accept()
     print("[rs]: Got a connection request from a client at {}".format(addr))
-    
+   
+    #create a list temporarily to hold domain names from client
+    domain_list = []
     with csockid:
-        print("Connected by ", csockid)
+        print("Connected by ", addr)
         while True:
-            data_from_client = csockid.recv(1024).decode("ascii")
+            data_from_client = csockid.recv(1024).decode('utf-8')
             print("[rs]: data received from client: {}".format(data_from_client))
+            domain_list.append(data_from_client)
             if not data_from_client:
                 break
+    print(domain_list)
 """
 if __name__ == "__main__":
     t1 = threading.Thread(name="server", target=server)

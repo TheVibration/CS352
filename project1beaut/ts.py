94f68c642ef50df6685405c80d92bc56bcd8fab0
Harsh <Harshabar9@gmail.com>
2020-02-26 13:45:00 -0500

    add ts.py

---
+++ project1beaut/ts.py
@@ -0,0 +1,37 @@
+import threading
+import time
+import random
+import sys
+import socket
+
+tsListenPort = sys.argv[1]
+tsListenPort = int(tsListenPort)
+
+def server():
+    try:
+        ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[TS]: Server socket created")
+    except socket.error as err:
+        print('socket open error: {}\n',format(err))
+        exit()
+
+    # get top server host name
+    tsServerHost = socket.gethostname()
+    print("[TS]: top server name is {}".format(tsServerHost))
+
+    # get top server ip address
+    tsHostip = socket.gethostbyname(tsServerHost)
+    print("[TS] root server ip is {}".format(tsHostip))
+  
+    # bind host,port
+    rs.bind((tsServerHost, tsListenPort))
+
+    # top server listens
+    ts.listen(5)
+
+    # accept incoming connections
+    csockid, addr = ts.accept()
+    print("[RS]: Got a connection request from a client at {}\n".format(addr))
+
+    
+
